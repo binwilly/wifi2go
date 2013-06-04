93872a80ca9bf0a92702197714d5e78e94b1aea1
Binwilly <binwilly@gmail.com>
2013-06-04 19:58:16 -0300

    service +1

--- service.py
+++ service.py
@@ -12,7 +12,7 @@ class Wifi(ndb.Model):
     date_added = ndb.DateTimeProperty(auto_now_add=True)
 
 
-class AccessPoints(webapp2.RequestHandler):
+class AccessPointsRequest(webapp2.RequestHandler):
 
     def get(self):
         d = {
@@ -24,8 +24,49 @@ class AccessPoints(webapp2.RequestHandler):
         self.response.write(json.dumps([d, d, d]))
 
     def post(self):
-        pass
+        data = json.loads(self.request.body)
+
+        try:
+            near_locations = self.findNearLocations(data['latitude'], data['longitude'])
+        except KeyError, e:
+            self.response.write('Error: latitude or longitude param missing')
+
+        near_wifis = self.findWifiByLocations(near_locations)
+
+        if len(near_wifis) == 0:
+            self.response.write('Wifi not found')
+        else:
+            self.response.write(json.dumps(near_wifis))
+
+
+    def findNearLocations(self, latitude, longitude):
+        locations = [(999, 991)]
+
+        return locations
+
+    def findWifiByLocations(self, locations):
+
+        wifi_results = [{
+            'name': 'Test Wi-Fi',
+            'password': 1234,
+            'location_id': 999
+        },
+        {
+            'name': 'Wi-Fi La Nona',
+            'password': 334,
+            'location_id': 991
+        }]
+
+        return wifi_results
+
+
+class AccessPointAdd(webapp2.RequestHandler):
+
+    ''' post param latitude and longitude'''
+    def post(self):
+        return None
 
 application = webapp2.WSGIApplication([
-    ('/', AccessPoints),
+    ('/api/1/wifi', AccessPointsRequest),
+    ('/api/1/addwifi', AccessPointAdd),
 ], debug=True)
