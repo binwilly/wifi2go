8cf487898171696f80854f003543c7a61d4b6106
Nicolas Ameghino <nicolas.ameghino@gmail.com>
2013-06-04 14:59:37 -0300

    First API draft for service

--- service.py
+++ service.py
@@ -1,11 +1,21 @@
 from google.appengine.api import users
 import cgi
 import webapp2
+import json
 
 class AccessPoints(webapp2.RequestHandler):
     def get(self):
-        self.response.write("#fafafa")
+    	d = {
+    		'name': 'Test Wi-Fi',
+    		'password': '1234',
+    		'location_id': 999,
+    	}
+
+        self.response.write(json.dumps([d, d, d]))
+
+    def post(self):
+    	pass
 
 application = webapp2.WSGIApplication([
-    ('/ap', AccessPoints),
+    ('/', AccessPoints),
 ], debug=True)
