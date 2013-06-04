4a853998e7f6d21deb6ef6aaa69f0683ae2fc91b
Nicolas Ameghino <nicolas.ameghino@gmail.com>
2013-06-04 15:09:06 -0300

    Added iOS skeleton project

---
+++ WiFi2Go-ios/WiFi2Go-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WiFi2Go-ios
+//
+//  Created by Nicolas Ameghino on 6/4/13.
+//  Copyright (c) 2013 Nicolas Ameghino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WiFi2Go-ios/WiFi2Go-ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WiFi2Go-ios
+//
+//  Created by Nicolas Ameghino on 6/4/13.
+//  Copyright (c) 2013 Nicolas Ameghino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
