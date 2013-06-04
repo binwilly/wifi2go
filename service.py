eb3e96636f3f3380ebabe7d6c2b19bac7f64e0b6
Binwilly <binwilly@gmail.com>
2013-06-04 15:30:59 -0300

    Wifi model

--- service.py
+++ service.py
@@ -1,20 +1,30 @@
-from google.appengine.api import users
+from google.appengine.ext import ndb
+
 import cgi
 import webapp2
 import json
 
+
+class Wifi(ndb.Model):
+
+    ssid = ndb.StringProperty(indexed=True)
+    password = ndb.StringProperty()
+    date_added = ndb.DateTimeProperty(auto_now_add=True)
+
+
 class AccessPoints(webapp2.RequestHandler):
+
     def get(self):
-    	d = {
-    		'name': 'Test Wi-Fi',
-    		'password': '1234',
-    		'location_id': 999,
-    	}
+        d = {
+            'name': 'Test Wi-Fi',
+            'password': '1234',
+            'location_id': 999,
+        }
 
         self.response.write(json.dumps([d, d, d]))
 
     def post(self):
-    	pass
+        pass
 
 application = webapp2.WSGIApplication([
     ('/', AccessPoints),
