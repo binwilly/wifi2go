bd7dc97fd02b5d036c5dac7ddcdd5e2b0d79ad6e
Nicolas Ameghino <nicolas.ameghino@gmail.com>
2013-06-07 17:14:29 -0300

    Added a basic generator for random venues for testing

--- WiFi2Go-ios/WiFi2Go-ios/MockWiFi2GoService.m
+++ WiFi2Go-ios/WiFi2Go-ios/MockWiFi2GoService.m
@@ -7,7 +7,37 @@
 //
 
 #import "MockWiFi2GoService.h"
+#import "Venue.h"
 
 @implementation MockWiFi2GoService
+
+NSUInteger getrnd(int limit) {
+    return arc4random() % limit;
+}
+
+float getfrnd() {
+    return rand() / (float) RAND_MAX;
+}
+
+-(void)queryWiFiForLatitude:(double)latitude longitude:(double)longitude completionBlock:(WiFi2GoServiceWiFiQueryComplete)block {
+    NSUInteger limit = 50;
+    NSUInteger size = getrnd(limit);
+    NSMutableArray *r = [NSMutableArray arrayWithCapacity:size];
+    
+    for (int i=0; i < size; ++i) {
+        NSDictionary *d =
+        @{
+          @"venue_id": [NSString stringWithFormat:@"random venue #%d", getrnd(1000)],
+          @"ssid": [NSString stringWithFormat:@"random wifi ssid #%d", getrnd(1000)],
+          @"password": getfrnd() < 0.4 ? [NSNull null] : @"hackme"
+        };
+        [r addObject:[Venue venueWithDictionary:d]];
+    }
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        sleep(1);
+        block(r, nil);
+    });
+}
 
 @end
