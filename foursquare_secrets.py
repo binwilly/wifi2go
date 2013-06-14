2740eef12f07318bf9db29bec7f66dd27dc9f410
Binwilly <binwilly@gmail.com>
2013-06-14 16:16:14 -0300

    foursquare integration

---
+++ foursquare_secrets.py
@@ -0,0 +1,7 @@
+# Copy this file to 'foursquare_secrets.py' FIRST, then fill it out.
+
+SECRETS = {
+	'client_id': '',
+  	# OAuth client secret. Must match what you set at https://foursquare.com/oauth.
+  	'client_secret': '',
+}
