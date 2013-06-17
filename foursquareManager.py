f10aea84902492e65347e673c6868032d5653cad
Binwilly <binwilly@gmail.com>
2013-06-17 11:44:47 -0300

    issue fixed

--- foursquareManager.py
+++ foursquareManager.py
@@ -7,8 +7,8 @@ API_VERSION = u'20130613'
 
 def getVenusNearby(ll, limit=None):
 
-    if limit == None:
-        limit = 30
+    
+    limit = 30
     uri = 'https://api.foursquare.com/v2/venues/search?ll=%s&client_id=%s&client_secret=%s&v=%s&limit=%s'
     url = uri % (ll, SECRETS['client_id'], SECRETS['client_secret'], API_VERSION, limit)
     return fetchJson(url)
